bc5ba36195bcdddd1238d16ffb2e32856a4c489d
John <jeacevedo92@utp.edu.co>
2017-10-04 14:15:09 -0500

    Image gray scale

---
+++ SampleOpenCv/CMakeFiles/opencvTry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencvTry.dir/opencvTry.cpp.o"
+  "opencvTry.pdb"
+  "opencvTry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencvTry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
