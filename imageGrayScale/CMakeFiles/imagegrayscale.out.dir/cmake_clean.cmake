928de5575f1961239b79ec088f5807531a53f370
John <jeacevedo92@utp.edu.co>
2017-10-06 23:25:27 -0500

    gray

---
+++ imageGrayScale/CMakeFiles/imagegrayscale.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imagegrayscale.out.dir/imagegrayscale.out_generated_imagegrayscale.cu.o"
+  "imagegrayscale.out.pdb"
+  "imagegrayscale.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/imagegrayscale.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
