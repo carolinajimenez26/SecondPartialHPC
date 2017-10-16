fe501267dc82d3ac150a5c1a4f53de0cf1c07d88
jeacevedo92 <jeacevedo92@utp.edu.co>
2017-10-15 19:09:20 -0500

    times graphic

--- SobelFIlter/CPU/c++/SobelCpu.sh
+++ SobelFIlter/CPU/c++/SobelCpu.sh
@@ -12,4 +12,13 @@ export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRAR
 
 export CUDA_VISIBLE_DEVICES=0
 
-./build/SobelFilter.out ../../../images/thor.jpg
+#./build/SobelFilter.out ../../../images/thor.jpg
+
+for i in {1..10}
+do
+	for j in {1..20}
+	do
+		./build/SobelFilter.out ../../../images/image$i.jpg >> times.txt
+	done
+	echo "Ready for image image$i.jpg"
+done
