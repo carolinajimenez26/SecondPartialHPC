e6d5d2cf3f1d4a22d4e927ab9aeaf055058f16e4
John <jeacevedo92@utp.edu.co>
2017-10-19 08:26:25 -0500

    shared mem

--- SobelFIlter/GPU/sharedMem/sharedMem.sh
+++ SobelFIlter/GPU/sharedMem/sharedMem.sh
@@ -12,4 +12,14 @@ export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRAR
 
 export CUDA_VISIBLE_DEVICES=0
 
-./sharedMem.out link.png 
+#./sharedMem.out link.png 
+
+for i in {1..10}
+do
+	for j in {1..20}
+	do
+		./build/sharedMem.out ../../../images/image$i.jpg >> times.txt
+	done
+	echo "Ready for image img$i.jpg"
+done
+
